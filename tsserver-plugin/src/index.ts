e13035c64b98445c022abbb0c6604b3a6ffb9c4a
Max Glass <glassmax13@gmail.com>
2023-03-22 11:46:09 -0400

    Add some logging to the language server

--- tsserver-plugin/src/index.ts
+++ tsserver-plugin/src/index.ts
@@ -14,6 +14,23 @@ function init(modules: { typescript: typeof ts }) {
   function create(info: ts.server.PluginCreateInfo) {
     pluginInfo = info;
 
+    function log(message: string) {
+      info.project.projectService.logger.info(message);
+    }
+
+    log(`Starting Chattriggers Typescript plugin ${info.project.getCurrentDirectory()}`);
+
+    function setupDecorator() {
+      // Set up decorator object
+      const proxy = Object.create(null);
+      for (let k of Object.keys(info.languageService)) {
+        const x = (info.languageService as any)[k];
+        proxy[k] = (...args: Array<{}>) => x.apply(info.languageService, args);
+      }
+      return proxy as typeof info.languageService;
+    }
+    const proxy = setupDecorator();
+
     const getScriptFileNames = info.languageServiceHost.getScriptFileNames.bind(
       info.languageServiceHost,
     );
@@ -26,7 +43,7 @@ function init(modules: { typescript: typeof ts }) {
       return scriptFileNames;
     };
 
-    return info.languageService;
+    return proxy;
   }
 
   function onConfigurationChanged(cfg: PluginConfig) {
